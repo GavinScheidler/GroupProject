d18c66f56272e4b8e502e291c60919c880046247
GavinScheidler <155492510+GavinScheidler@users.noreply.github.com>
2025-03-05 14:32:40 -0600

    Updated Recommendations

--- Rules& Rec Gavin/IDS52.java
+++ Rules& Rec Gavin/IDS52.java
@@ -3,31 +3,26 @@
  * Author: Gavin Scheidler
 */
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 public class IDS52 {
 
-    private static void evalScript(String firstName) throws ScriptException {
+    private static void evalScript(String input) throws ScriptException {
         // Allow only alphanumeric and underscore chars in firstName
         // (modify if firstName may also include special characters)
-        if (!firstName.matches("[\\w]*")) {
+        if (!input.matches("[\\w]*")) {
             // String does not match whitelisted characters
             throw new IllegalArgumentException();
         }
-
-        ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("javascript");
-        engine.eval("print('" + firstName + "')");
+        System.out.println("No script Detected");
     }
 
     public static void main(String[] args) {
 
         try {
+            evalScript("hello");
             evalScript("<somescript>");
         } catch (ScriptException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
